85bb9f5cb5e59284898a13d17aedea2dfff69744
Owais Ibrahim <owaisibrahim99@gmail.com>
2019-11-10 15:59:14 +0500

    Initial Project with Navigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const SearchScreen  = () => {
+    return (
+        <Text>SearchScreen</Text>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default SearchScreen;
